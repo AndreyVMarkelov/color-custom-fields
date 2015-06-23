b0473fda6b7af3dad8a953d54d3b8a625eed581b
Маркелов Андрей Валентинович <AVMarkelov.SBT@sberbank.ru>
2015-06-23 19:27:25 +0300

    fix #2

--- src/main/java/ru/andreymarkelov/atlas/plugins/jira/colorfields/ColorPickerField.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/jira/colorfields/ColorPickerField.java
@@ -1,14 +1,15 @@
 package ru.andreymarkelov.atlas.plugins.jira.colorfields;
 
-import com.atlassian.jira.issue.customfields.impl.TextCFType;
-import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
-import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
-import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.fields.CustomField;
-import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
 import java.util.Map;
 
-public class ColorPickerField extends TextCFType {
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.customfields.impl.GenericTextCFType;
+import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
+import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
+
+public class ColorPickerField extends GenericTextCFType {
     public ColorPickerField(CustomFieldValuePersister customFieldValuePersister, GenericConfigManager genericConfigManager) {
         super(customFieldValuePersister, genericConfigManager);
     }
