9558ce8d1215eedd4c0d705f9bd5aa36d193e3dc
amarkelov <andrey.markelov@infobip.com>
2017-04-30 22:32:26 +0300

    fix confluence view

--- src/main/java/ru/andreymarkelov/atlas/plugins/jira/colorfields/ColorPickerField.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/jira/colorfields/ColorPickerField.java
@@ -1,14 +1,9 @@
 package ru.andreymarkelov.atlas.plugins.jira.colorfields;
 
-import java.util.Map;
-
-import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.customfields.impl.GenericTextCFType;
 import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
 import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
-import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.TextFieldCharacterLengthValidator;
-import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 
 public class ColorPickerField extends GenericTextCFType {
@@ -18,13 +13,5 @@ public class ColorPickerField extends GenericTextCFType {
             TextFieldCharacterLengthValidator textFieldCharacterLengthValidator,
             JiraAuthenticationContext jiraAuthenticationContext) {
         super(customFieldValuePersister, genericConfigManager, textFieldCharacterLengthValidator, jiraAuthenticationContext);
-    }
-
-    @Override
-    public Map<String, Object> getVelocityParameters(
-            final Issue issue,
-            final CustomField field,
-            final FieldLayoutItem fieldLayoutItem) {
-        return super.getVelocityParameters(issue, field, fieldLayoutItem);
     }
 }
